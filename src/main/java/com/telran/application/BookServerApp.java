3d7ff421a23e41b88e678866a608c1e485b46d53
Shveta Sachdeva <sshveta@redhat.com>
2025-10-13 13:15:09 -0700

    Added an import for testing
    Signed-off-by: Shveta Sachdeva <sshveta@redhat.com>

--- src/main/java/com/telran/application/BookServerApp.java
+++ src/main/java/com/telran/application/BookServerApp.java
@@ -2,6 +2,7 @@ package com.telran.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.weblogic.foo.Bar;
 
 @SpringBootApplication
 public class BookServerApp {
